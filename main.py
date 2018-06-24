bc8962998303d87c671ae94b498c9206583da53e
Kaan Goksal <kaangoksal@gmail.com>
2018-06-24 11:08:04 -0400

    Cleaned unneccesary code

--- main.py
+++ main.py
@@ -8,20 +8,14 @@ class MyFirstGUI(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-        self.label = Label(self, text="This is our first GUI!")
+        self.label = Label(self, text="Weather widget sample ")
         self.label.grid(column=1,row=1)
 
-        self.greet_button = Button(self, text="Greet", command=self.greet)
-        self.greet_button.grid(column=1,row=2)
-
-        self.close_button = Button(self, text="Close", command=self.quit)
-        self.close_button.grid(column=1,row=3)
 
         self.weather_widget = WeatherWidget(self)
         self.weather_widget.grid(column=1, row=4, pady=(10,10),padx=(10,10))
 
-    def greet(self):
-        print("Greetings!")
+        # TODO Intialize weather widget controller and apis
 
 
 my_gui = MyFirstGUI()
